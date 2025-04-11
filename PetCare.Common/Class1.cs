e0e842fc4f6a460a8217875e3caac708fac3e756
Student <Student@206L-01>
2025-04-11 11:52:20 +0300

    Lab1 Task 1 (Створіть свою ВЛАСНУ (не як у прикладі) модель(набір класів) на довільну тематику із 4 або більше класів, серед яких хоча б двоє класів наслідуються від якихось інших. Результуючі класи повинні знаходитись у проєкті {Назва тематики}.Common. Проєкти створювати всередині рішення(.sln) {Назва тематики}.)

---
+++ PetCare.Common/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace PetCare.Common
+{
+    public class Animal
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Species { get; set; }
+    }
+    public class Dog : Animal
+    {
+        public string Breed { get; set; }
+        public string ActivityLevel { get; set; }
+    }
+    public class Cat : Animal
+    {
+        public string Furtype { get; set; }
+        public bool IsIndependent { get; set; }
+    }
+    public class Owner
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Animal> Pets { get; set; } = new List<Animal>();
+    }
+}
