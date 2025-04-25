e8dee781cbdc2f2920f35ef56f6e51f9b2e7afde
245835-gh <antygrave@gmail.com>
2025-04-25 11:15:44 +0300

    Lab2 Task1 ---->(no finish)

---
+++ PetCare.Common/Services/CrudServiseAsync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCare.Common.Services
+{
+    public class CrudServiseAsync<T> : ICrudServiseAsync<T> where T : class
+    {
+        private readonly ConcurentDictionary<Guid, T> _storage = new();
+        private readonly Func<T,Guid> _getId;
+        public CrudServiceAsync()
+        {
+            var prop = typeof(T).GetProperty("Id");
+            if ( prop == null || prop.PropertyType != typeof(Guid))
+            {
+                throw new InvalidOperationException("Клас повинен мати властивість ID типу Guid");
+                _getId = (T obj) => (Guid)prop.GetValue(obj);
+            }
+        }
+    }
+}
---
+++ PetCare.Common/Services/ICrudServiseAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCare.Common.Services
+{
+    public interface ICrudServiseAsync<T>: IEnumerable<T>
+    {
+        Task<bool> CreateAsync(T element);
+        Task<T> ReadAsync(Guid id);
+        Task<IEnumerable<T>> ReadAllAsync();
+        Task<IEnumerable<T>> ReadAllAsync(int pasge, int amount);
+        Task<bool> UpdateAsync(T element);
+        Task<bool> RemoveAsync(T element);
+        Task<bool> SaveAsync(string filePath);
+
+    }
+}
